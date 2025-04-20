ce9e444a1bbeab39203220bcda81aa591cdf8a17
PhysiqueRUSH <physiquerush@gmail.com>
2025-04-20 23:26:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrémente la version du cache pour forcer la mise à jour
-const CACHE_NAME = 'push-up-odyssey-cache-v20';
+const CACHE_NAME = 'push-up-odyssey-cache-v2.0';
 
 // Liste de tous les fichiers qu’on veut précacher
 const urlsToCache = [
